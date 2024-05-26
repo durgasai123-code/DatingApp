71532c109d01683568f392a9e97b2fbca2b977c1
Kandregula Durga Sai <kdurgas@apac.corpdir.net>
2024-05-27 00:55:56 +0530

    initial commit

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //api/user
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UserController(DataContext context){
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUsers>>> GetUsers(){
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")] //api/user/1
+    public async Task<ActionResult<AppUsers>> GetUser(int id)
+    {
+        if(id>= 0)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if(user!= null){
+                return user;
+            }
+            else{
+                return NotFound();
+            }
+        }
+        else{
+            return NotFound();
+        }
+    } 
+
+
+}
